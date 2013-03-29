2a443d31fee220a61e9b9b22e6c738a9489b9908
Robert Sikorski <sikorski.rob@gmail.com>
2013-03-28 22:57:14 -0400

    Almost done with map making, gameplay coming soon

--- src/LPTiledMap.js
+++ src/LPTiledMap.js
@@ -13,7 +13,7 @@ var LPTiledMap = function() {
 
 	this.mapWidth = -1;
 	this.mapHeight = -1;
-	this.tileValues = new Array(10);
+	this.tileValues = new Array(10); // why doesn't this work?
 }
 
 LPTiledMap.prototype.loadMap = function(mapName) {
@@ -21,12 +21,12 @@ LPTiledMap.prototype.loadMap = function(mapName) {
 	xmlHR.onload = function() {
 		var rootChildNodesLength = xmlHR.responseXML.documentElement.childNodes.length;
 
-		for (var i = 0; i < rootChildNodesLength; i = i + 1) {
+		for (var i = 0; i < rootChildNodesLength; i++) {
 			var tempNode = xmlHR.responseXML.documentElement.childNodes[i];
 
 			if (tempNode.nodeName == "tileset") {
 				var attributesLength = tempNode.attributes.length;
-				for (var j = 0; j < attributesLength; j = j + 1) {
+				for (var j = 0; j < attributesLength; j++) {
 					var tempAttribute = tempNode.attributes[j];
 					if (tempAttribute.name == "name") {
 						console.log("1");
@@ -40,7 +40,7 @@ LPTiledMap.prototype.loadMap = function(mapName) {
 				}
 			} else if (tempNode.nodeName == "layer") {
 				var attributesLength = tempNode.attributes.length;
-				for (var j = 0; j < attributesLength; j = j + 1) {
+				for (var j = 0; j < attributesLength; j++) {
 					var tempAttribute = tempNode.attributes[j];
 					if (tempAttribute.name == "name") {
 						console.log("tA.name = " + tempAttribute.name);
@@ -55,20 +55,37 @@ LPTiledMap.prototype.loadMap = function(mapName) {
 					this.tileValues = new Array(10);
 					this.tileValues[i] = new Array(this.mapWidth * this.mapHeight); // multidimensional magic
 				}
+
 				// now loop to create array with info for map.
 				var layerLength = tempNode.childNodes.length;
-				for (var j = 0; j < layerLength; j = j + 1) {
+				for (var j = 0; j < layerLength; j++) {
 					var tempDataNode = tempNode.childNodes[j];
-					console.log("line 65"); // getting closer! woo hoo!
-					if (tempDataNode.name == "data") {
-						console.log("line 67");
+					if (tempDataNode.nodeName == "data") {
 						var dataLength = tempDataNode.childNodes.length;
-						for (var k = 0; k < dataLength; k = k +1) {
-							console.log(tempDataNode.childNodes[k].name);
+						for (var k = 0; k < dataLength; k++) {
+							if (tempDataNode.childNodes[k].nodeName == "tile") {
+								var tempTileNode = tempDataNode.childNodes[k];
+								var tempTileNodeAttibuteLength = tempTileNode.attributes.length;
+								for (var l = 0; l < tempTileNodeAttibuteLength; l++) {
+									console.log("gid:" + tempTileNode.attributes[l].value);
+									this.tileValues[i][l] = tempTileNode.attributes[l].value;
+								}
+							}
 						}
 					}
 				}
 			}
+
+			if (this.tileValues) {
+				for (var i = 0; i < this.tileValues.length; i++) {
+					//for (var j = 0; j < this.tileValues[i].length; j++) {
+					//	if (this.tileValues[i][j]) {
+					//		console.log("m:" + this.tileValues[i][j]);
+					//	}
+					//}
+				}
+			}
+
 
 		}
 
